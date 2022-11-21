5ce7a4ec7da9b41a4604cdff7e0f6ea8f7aed507
leegyeongwhan <leekhy02@naver.com>
2022-11-21 21:11:37 +0900

    feat(week4 미션1): 미션1 map
    map 을 활용하여 key 값을 확인하여 아이디값 변경

---
+++ src/main/java/com/week4/mission1/shopping/MemberShipMangementMap.java
@@ -0,0 +1,44 @@
+package com.week4.mission1.shopping;
+
+import com.week4.mission1.shopping.membership.GRADE;
+import com.week4.mission1.shopping.membership.MemberShip;
+
+import java.util.*;
+
+public class MemberShipMangementMap {
+
+    private final Map<Integer, MemberShip> memberShips;
+
+    public MemberShipMangementMap() {
+        this.memberShips = new HashMap<>();
+    }
+
+    public void addMemberShip(int id, String name, GRADE grade) {
+        MemberShip memberShip = new MemberShip(id, name, grade);
+        if (memberShips.containsKey(memberShip.getId())) {
+            memberShips.replace(memberShip.getId(), memberShip);
+            System.out.println("이미 있는 아이디" + memberShip.getId() + " 의값이 변경됩니다");
+            return;
+        }
+        this.memberShips.put(memberShip.getId(), memberShip);
+    }
+
+    public void showAllMember() {
+        System.out.println("=========================");
+        for (Map.Entry<Integer, MemberShip> entry : memberShips.entrySet()) {
+            System.out.println(entry.getValue());
+        }
+        System.out.println("=========================");
+    }
+
+    public boolean removeMember(int index) {
+        for (Integer integer : memberShips.keySet()) {
+            if (index == integer) {
+                memberShips.remove(index);
+                return true;
+            }
+        }
+        System.out.println(index + "에 해당하는 맴버가 없습니다");
+        return false;
+    }
+}
